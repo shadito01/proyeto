51c4c58686321554b8fd046ae4a9c3c3cde8fa99
Jeffry Marte Bonifacio <frijey@hotmail.com>
2018-02-21 22:26:45 -0400

    Some new changes
    Algunos cambios nuevos

--- pagina web/Scripts/App.js
+++ pagina web/Scripts/App.js
@@ -1,20 +1,19 @@
 
 const nombre = document.querySelector("#NOMBRE");
 const apellido = document.querySelector("#APELLIDO");
-const columna1 = document.querySelector("#columna1f1");
-const columna2 = document.querySelector("#columna2f1");
+const columna1f1 = document.querySelector("#columna1f1");
+const columna2f1 = document.querySelector("#columna2f1");
+const columna3f1 = document.querySelector("#columna3f1");
+const columna4f1 = document.querySelector("#columna4f1");
+const columna5f1 = document.querySelector("#columna5f1");
+const columna6f1 = document.querySelector("#columna6f1");
 const botonagregar = document.querySelector("#botonagregar");
 
-var firebaseChanguinref = firebase.database().ref().child("estudiantes").child("1").child("nombre");
-var firebaseChanguinref1 = firebase.database().ref().child("estudiantes").child("1").child("apellido");
+var firebaseChanguinref = firebase.database().ref().child("estudiantes").child("1");
 
 firebaseChanguinref.on('value', (snapshot) => {
-    columna1.textContent = snapshot.val();
-    console.log("I Have Retreive The Text from Firebase");
-});
-
-firebaseChanguinref1.on('value', (snapshot) => {
-    columna2.textContent = snapshot.val();
+    columna1f1.textContent = snapshot.child("nombre").val();
+    columna2f1.textContent = snapshot.child("apellido").val();
     console.log("I Have Retreive The Text from Firebase");
 });
 
